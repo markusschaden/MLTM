461107eacd2128efdb9d26c7e28412b915375fc0
Stefan Gugger <stegu_@hotmail.de>
2015-05-03 01:20:33 +0200

    updated logic

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,7 +17,7 @@ function MLTM(tag) {
   var proportion;
   this.__defineGetter__("proportion", function() { return proportion; });
   this.__defineSetter__("proportion", function(value) {
-    if (value < 1 && value >= 0) {
+    if (value <= 1 && value > 0) {
       proportion = value;
       recalcValues();
     }
@@ -32,6 +32,15 @@ function MLTM(tag) {
       }
   });
 
+  var extraSpace;
+  this.__defineGetter__("extraSpace", function() { return extraSpace; });
+  this.__defineSetter__("extraSpace", function(value) {
+      if (value >= 0) {
+        extraSpace = value;
+        recalcValues();
+      }
+  });
+
   var distances;
   this.__defineGetter__("distances", function() {
     return distances;
@@ -42,50 +51,55 @@ function MLTM(tag) {
     return innersizes;
   });
 
-  var outersizes;
-  this.__defineGetter__("outersizes", function() {
-    return outersizes;
+  var controlsize;
+  this.__defineGetter__("controlsize", function() {
+    return controlsize;
   });
 
   function init() {
     maxsize = 100;
-    proportion = 0.20;
+    proportion = 0.80;
     limit = 3;
+    extraSpace = 50;
     recalcValues();
   }
 
   function recalcValues() {
-    innersizes = [maxsize];
-    outersizes = new Array();
+    innersizes = [maxsize+extraSpace];
+    controlsize = 0;
     for (var i = 0; i < limit; i++) {
-      outersizes.push(((i > 0) ? outersizes[i-1] : 0)+Math.pow(2,i)*innersizes[i]);
-      innersizes[i+1] = innersizes[i]-innersizes[i]*proportion
+      controlsize += Math.pow(2,i)*innersizes[i];
+      innersizes.push(innersizes[i]*proportion);
     }
 
     distances = new Array();
-    for (var i = limit-1; i >= 0; i--)
-      distances.push((outersizes[i]-maxsize)/4+maxsize/2);
+    for (var i = 0; i < limit-1; i++)
+      distances.push((((i > 0) ? 2*distances[i-1]-innersizes[i-1] : controlsize)-innersizes[i])/4+innersizes[i]/2)
 
     console.log("maxsize: "+maxsize);
     console.log("proportion: "+proportion);
     console.log("limit: "+limit);
     console.log("innersizes: "+innersizes);
-    console.log("outersizes: "+outersizes);
+    console.log("controlsize: "+controlsize);
     console.log("distances: "+distances);
-  }
-
-  function calcSize(sub) {
-    return maxsize-sub*maxsize*proportion;
   }
 
   init();
 }
 
-function Node(tag, mltm) {
+function Node(tag, valueTag, mltm, sub) {
   this.tag = tag;
+  this.valueTag = valueTag;
   this.mltm = mltm;
-
+  this.sub = sub;
+  this.sel = sub;
   this.collapsed = false;
+  this.x = 0;
+  this.y = 0;
+}
+
+function calcSizeBySelection(node) {
+  return Math.pow(node.mltm.proportion,node.sel);
 }
 
 function updateSelectedNode(index, mltm) {
@@ -93,58 +107,59 @@ function updateSelectedNode(index, mltm) {
   if (!node) node = $(mltm.tag.children("[node]")[0]).data("node");
   var height = mltm.tag.height();
   var width = mltm.tag.width();
-  node.tag.velocity({
-    top: Math.round(height/2-mltm.maxsize/2)+"px",
-    left: Math.round(width/2-mltm.maxsize/2)+"px",
+  node.x = Math.round(width/2-mltm.maxsize/2);
+  node.y = Math.round(height/2-mltm.maxsize/2);
+  node.valueTag.velocity({
+    top: node.y+"px",
+    left: node.x+"px",
   });
   var maxsize = mltm.maxsize;
-  node.tag.velocity({
+  node.valueTag.velocity({
     width: maxsize+"px",
     height: maxsize+"px",
-    scaleX: 1,
-    scaleY: 1
+    scaleX: calcSizeBySelection(node),
+    scaleY: calcSizeBySelection(node)
   }, { queue: false });
-  var sub = 0;
-  if (sub >= node.mltm.limit) {
-    node.tag.velocity("fadeOut", { queue: false });
-    node.tag.velocity({
+  if (node.sub >= node.mltm.limit) {
+    node.valueTag.velocity("fadeOut", { queue: false });
+    node.valueTag.velocity({
       scaleX: 0,
       scaleY: 0
     }, { queue: false });
-  } else if (!node.tag.is(":visible"))
-    node.tag.velocity("fadeIn", { queue: false });
+  } else if (!node.valueTag.is(":visible"))
+    node.valueTag.velocity("fadeIn", { queue: false });
   var nodes = node.tag.children("[node]");
   for (var i = 0; i < nodes.length; i++)
-    updateNodes(i, nodes[i], nodes.length, sub+1);
+    updateNodes(i, nodes[i], nodes.length, node);
 }
 
-function updateNodes(index, tag, count, sub) {
+function updateNodes(index, tag, count, parent) {
   var node = $(tag).data("node");
-  var x = Math.cos(((360/count)*(index+1))* Math.PI / 180.0)*node.mltm.distances[sub-1];
-  var y = Math.sin(((360/count)*(index+1))* Math.PI / 180.0)*node.mltm.distances[sub-1];
-  node.tag.velocity({
-    top: y+"px",
-    left: x+"px",
+  node.x = parent.x+Math.cos(((360/count)*(index+1))* Math.PI / 180.0)*node.mltm.distances[node.sub-1];
+  node.y = parent.y+Math.sin(((360/count)*(index+1))* Math.PI / 180.0)*node.mltm.distances[node.sub-1];
+  node.valueTag.velocity({
+    top: node.y+"px",
+    left: node.x+"px",
   });
   var maxsize = node.mltm.maxsize;
-  node.tag.velocity({
+  node.valueTag.velocity({
     width: maxsize+"px",
     height: maxsize+"px",
-    scaleX: 1-node.mltm.proportion,
-    scaleY: 1-node.mltm.proportion
+    scaleX: calcSizeBySelection(node),
+    scaleY: calcSizeBySelection(node)
   }, { queue: false });
-  if (sub >= node.mltm.limit) {
-    node.tag.velocity("fadeOut", { queue: false });
-    node.tag.velocity({
+  if (node.sub >= node.mltm.limit || parent.collapsed) {
+    node.valueTag.velocity("fadeOut", { queue: false });
+    node.valueTag.velocity({
       scaleX: 0,
       scaleY: 0
     }, { queue: false });
-  } else if (!node.tag.is(":visible"))
-    node.tag.velocity("fadeIn", { queue: false });
+  } else if (!node.valueTag.is(":visible"))
+    node.valueTag.velocity("fadeIn", { queue: false });
 
   var nodes = node.tag.children("[node]");
   for (var i = 0; i < nodes.length; i++)
-    updateNodes(i, nodes[i], nodes.length, sub+1);
+    updateNodes(i, nodes[i], nodes.length, node);
 }
 
 $(document).ready(function() {
@@ -155,16 +170,20 @@ $(document).ready(function() {
       if (tag.data("mltm") === undefined) tag.data("mltm", mltm);
       var nodes = tag.children("[node]");
       for (var i = 0; i < nodes.length; i++)
-        initNodes(nodes[i], mltm);
+        initNodes(nodes[i], mltm, 0);
   }
 
-  function initNodes(object, mltm) {
+  function initNodes(object, mltm, sub) {
     var tag = $(object);
-    var node = new Node(tag, mltm);
+    var node = new Node(tag, $(tag.children("[nodevalue]")[0]), mltm, sub);
+    tag.click({mltm: mltm, node: node}, function(event) {
+      event.data.mltm.selectedNodes[0] = event.data.node;
+      $.each(mltms, updateSelectedNode);
+    });
     if (tag.data("node") === undefined) tag.data("node", node);
     var nodes = tag.children("[node]");
     for (var i = 0; i < nodes.length; i++)
-      initNodes(nodes[i], mltm);
+      initNodes(nodes[i], mltm, sub+1);
   }
 
   $.each($.find("[mltm]"),initMLTMs);
